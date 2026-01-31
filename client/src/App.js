211738ebd9dd52b0d60aa18a12942493e1f502c0
DHURSHITHA <dhurshitiha.n2006@gmail.com>
2026-01-31 13:34:53 +0530

    updated correct google client id

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Dashboard from './components/Dashboard';
 import './App.css';
 const API=process.env.REACT_APP_URL;
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || 
-  "732537579525-0vh7jkpkhp5c8dt4k6fh1aelcu53hame.apps.googleusercontent.com";
+  "732537579525-3k1mbq9pkitmb8kldvdorbaro097sv4u.apps.googleusercontent.com";
 
 // const GOOGLE_CLIENT_ID = "732537579525-0vh7jkpkhp5c8dt4k6fh1aelcu53hame.apps.googleusercontent.com";
 
